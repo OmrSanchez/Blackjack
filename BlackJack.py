be6602dc431a5580deb7c4163f22e75659083395
OmrSanchez <omar.sanchez008@gmail.com>
2025-01-13 21:43:49 -0500

    It's looking good

--- BlackJack.py
+++ BlackJack.py
@@ -1,64 +1,39 @@
-import random
-
 import FreeSimpleGUI as gui
 import cards_dict
 from functions import *
 
 DECK_LIST = cards_dict.deck
-# CARD_DEST_LIST = extract_dest_and_values(DECK_LIST)[0]
-# CARD_VALUE_LIST = extract_dest_and_values(DECK_LIST)[1]
-
-deck = extract_dest_and_values(DECK_LIST)
-print(deck)
-print(deck[0])
+DECK = extract_dest_and_values(DECK_LIST)
 
 #Game display configs
-
 BACKGROUND_COLOR = "teal"
 GRAPH_COLOR = "green"
 SCOREBOARD_COLOR = "darkgrey"
-
-gui.set_options(font="Arial, 16", element_padding=(0,0), text_color="gold", border_width=0)
+START_SCORE = 0
+gui.set_options(font="Arial, 18", element_padding=(0,0), text_color="gold", border_width=0)
 gui.theme("DarkGrey12")
 
-# Game layout
+
 game_title_image = gui.Image(source="Cards Pack/BlackJack Logo.png", size=(200, 50), key="-GAME-TITLE-IMG-", pad=(0,0))
-start_button = gui.Button("START", key="-START-", mouseover_colors="gold")
-# exit_button = gui.Button("EXIT", key="-EXIT-", button_color="red")
+blackjack_button = gui.Button("BLACKJACK", key="-BLACKJACK-", mouseover_colors="gold")
+play_button = gui.Button("PLAY", key="-PLAY-", size=(10, 1), pad=(20, 10))
 menu_ace = gui.Image(source="Cards Pack/Deck/Spades 1.png", size=(100, 150), key="-MENU-ACE-", pad=(5,30))
 menu_jack = gui.Image(source="Cards Pack/Deck/Spades 11.png", size=(100, 150), key="-MENU-JACK-", pad=(5,30))
-
-# Player Controls
-hit_button = gui.Button("HIT", key="-HIT-", size=(10, 1), pad=(20, 10))
-hold_button = gui.Button("HOLD", key="-HOLD-", size=(10, 1), pad=(20, 10))
-# quit_button = gui.Button("QUIT", key="-QUIT-", button_color="red")
-play_again_button = gui.Button("PLAY AGAIN", key="-PLAY-AGAIN-BUTTON-", size=(10,1), pad=(0, 95))
-
-# Display both scores
-start_score = 0
+hit_button = gui.Button("HIT", key="-HIT-", size=(10, 1), pad=(20, 10), visible=False)
+hold_button = gui.Button("HOLD", key="-HOLD-", size=(10, 1), pad=(20, 10), visible=False)
+play_again_button = gui.Button("PLAY AGAIN", key="-PLAY-AGAIN-BUTTON-", size=(10, 1), pad=(20, 10), visible=False)
 game_title_image_start = gui.Image(source="Cards Pack/BlackJack Logo.png", size=(196, 50), key="-GAME-TITLE-IMG-START-", pad=(0,0))
-player_score = gui.Text(str(f"Your Score:\n\n        {start_score}"), text_color="white", key="-PLAYER-SCORE-", pad=(0,90))
-npc_score = gui.Text(str(f"Opponent Score:\n\n            {start_score}"), text_color="white", key="-NPC-SCORE-", pad=(0,80))
+player_score = gui.Text(str(f"Your Score:\n\n        {START_SCORE}"), text_color="white", key="-PLAYER-SCORE-", pad=(0,90))
+npc_score = gui.Text(str(f"Opponent Score:\n\n            {START_SCORE}"), text_color="white", key="-NPC-SCORE-", pad=(0,80))
 
 # Create window and manage layout as a graph.
-play_again_col = gui.Column([[play_again_button]],
-							key="-PLAY-AGAIN-",
-							visible=False,
-							pad=(0,0))
 
-scoreboard_col = gui.pin(gui.Column([[game_title_image_start], [npc_score], [player_score], [play_again_col]],
+scoreboard_col = gui.Column([[game_title_image_start], [npc_score], [player_score], [play_button]],
 							size=(198, 625),
 							element_justification="center",
 							key="-SCOREBOARD-",
 							visible=False,
-							pad=(10,0)))
-
-hit_hold_col = gui.Column([[hit_button, hold_button]],
-						  	element_justification="center",
-							key="-PLAYER-CONTROLS-",
-						    size=(500,50),
-						  	pad=(300, 0),
-						  	visible=False)
+							pad=(10,0))
 
 layout = [
 	[[gui.VPush()]],
@@ -77,17 +52,17 @@ layout = [
 		visible=False,
 	)],
 
-	[hit_hold_col],
+	[[gui.Push(), gui.Column([[hit_button, hold_button, play_again_button]]), gui.Push()]],
 
 	[gui.Push(), gui.Column([[menu_jack, menu_ace]], key="-MENU-CARDS-", visible=True, element_justification="center"),
 	 gui.Push()],
 
-	[gui.Push(), gui.Column([[start_button]]), gui.Push()],
+	[gui.Push(), gui.Column([[blackjack_button]]), gui.Push()],
 
 	[[gui.VPush()]],
 ]
 
-window = gui.Window("Blackjack", layout, size=(1100, 800), finalize=True, location=(100, 100))
+window = gui.Window("Blackjack", layout, size=(1000, 750), finalize=True, location=(100, 100))
 # size=(1022, 690)
 graph = window["-GRAPH-"]
 graph.draw_line((-250, 250), (250, 250), color="black", width=4)
@@ -106,7 +81,7 @@ player_score = 0
 player_cards = []
 npc_score = 0
 npc_cards = []
-
+print(DECK)
 while True:
 	event, values = window.read()
 	print(event, values)
@@ -116,33 +91,33 @@ while True:
 	elif event == '-QUIT-':
 		break
 
-	elif event == '-START-':
+	elif event == '-BLACKJACK-':
 		# Display game
 		window["-GAME-TITLE-IMG-"].update(visible=False)
 		window["-MENU-CARDS-"].update(visible=False)
-		window["-START-"].update(visible=False)
-
-		window["-PLAYER-CONTROLS-"].update(visible=True)
+		window["-BLACKJACK-"].update(visible=False)
 		window["-PLAYER-SCORE-"].update(visible=True)
 		window["-NPC-SCORE-"].update(visible=True)
 		window["-SCOREBOARD-"].update(visible=True)
 		window["-GRAPH-"].update(visible=True)
 
-		# Track Cards
-		player_cards = [get_card(deck) for count in range(2)]
-		print(f"Player Cards: {player_cards}")
+	elif event == '-PLAY-':
+		window["-HIT-"].update(visible=True)
+		window["-HOLD-"].update(visible=True)
+		window["-PLAY-"].update(visible=False)
 
-		npc_cards = [get_card(deck) for count in range(2)]
+		for i in range(2):
+			player_cards.append(get_card(DECK))
+			npc_cards.append(get_card(DECK))
+
+		print(f"Player Cards: {player_cards}")
 		print(f"NPC Cards: {npc_cards}")
 
-		# Track Scores
 		player_score = get_score(player_cards)
-		print(f"Player Score: {player_score}")
 		window["-PLAYER-SCORE-"].update(str(f"Your Score:\n\n        {player_score}"))
 
-		npc_score = get_score(npc_cards)
-		print(f"NPC Score: {npc_score}")
-		window["-NPC-SCORE-"].update(str(f"Your Score:\n\n        {npc_score}"))
+		npc_score = npc_cards[1]['value']
+		window["-NPC-SCORE-"].update(str(f"Opponent Score:\n\n           {npc_score}"))
 
 		for i in range(len(player_cards)):
 			graph.DrawImage(player_cards[i]['dest'], location=(PLAYER_X, PLAYER_Y))
@@ -153,14 +128,11 @@ while True:
 		graph.DrawImage(npc_cards[1]['dest'], location=(NPC_X + 20, NPC_Y + 20))
 
 	elif event == '-HIT-':
-		next_card = get_card(deck)
-		player_cards.append(next_card)
-
-		player_score += next_card['value']
+		player_cards.append(get_card(DECK))
+		player_score = get_score(player_cards)
 		window["-PLAYER-SCORE-"].update(str(f"Your Score:\n\n        {player_score}"))
 
 		graph.erase()
-
 		PLAYER_X = -40
 		PLAYER_Y = -60
 		for i in range(len(player_cards)):
@@ -172,39 +144,58 @@ while True:
 		graph.DrawImage(npc_cards[1]['dest'], location=(NPC_X + 20, NPC_Y + 20))
 
 	elif event == '-HOLD-':
-		window["-PLAYER-CONTROLS-"].update(visible=False)
-		window["-PLAY-AGAIN-"].update(visible=True)
-		npc_cards, npc_score_number, npc_index, npc_card_values_list = npc_hit(CARD_VALUE_LIST, DECK_DEST_LIST)
-		for i in npc_cards:
-			graph.DrawImage(i, location=(NPC_X, NPC_Y))
+		window["-HIT-"].update(visible=False)
+		window["-HOLD-"].update(visible=False)
+		window["-PLAY-AGAIN-BUTTON-"].update(visible=True)
+
+		graph.erase()
+
+		print(npc_cards)
+		npc_score = get_score(npc_cards)
+		print(f"NPC Score: {npc_score}")
+
+
+		if npc_score < 21 and 21 - npc_score > 4:
+			npc_cards.append(get_card(DECK))
+		elif npc_score < 21 and 21 - npc_score <= 4:
+			pass
+
+		npc_score = get_score(npc_cards)
+		window["-NPC-SCORE-"].update(str(f"Opponent Score:\n\n           {npc_score}"))
+
+		print(f"Player Cards: {player_cards}")
+		print(f"NPC Cards: {npc_cards}")
+
+		PLAYER_X = -40
+		PLAYER_Y = -60
+		NPC_X = -50
+		NPC_Y = 150
+		for i in range(len(player_cards)):
+			graph.DrawImage(player_cards[i]['dest'], location=(PLAYER_X, PLAYER_Y))
+			PLAYER_X += 20
+			PLAYER_Y += 20
+		for i in range(len(npc_cards)):
+			graph.DrawImage(npc_cards[i]['dest'], location=(NPC_X, NPC_Y))
 			NPC_X += 20
-		window["-NPC-SCORE-"].update(npc_score_number)
+			NPC_Y += 20
 
-		declare_result(current_sum, npc_score_number)
+		declare_result(player_score, npc_score)
 
-	# elif event == '-PLAY-AGAIN-BUTTON-':
-	# 	DECK_DEST_LIST = extract_dest_and_values(DECK_LIST)[0]
-	# 	window["-PLAYER-CONTROLS-"].update(visible=True)
-	# 	window["-PLAY-AGAIN-"].update(visible=False)
-	# 	window["-GRAPH-"].erase()
-	# 	player_cards = [select_cards(DECK_DEST_LIST)[0] for i in range(2)]
-	# 	init_index = store_index(player_cards, DECK_DEST_LIST)
-	# 	current_card_values_list = [CARD_VALUE_LIST[i] for i in init_index]
-	# 	for i in current_card_values_list:
-	# 		if check_ace(i):
-	# 			if (sum(current_card_values_list) - i) + 11 > 21:
-	# 				current_card_values_list.remove(i)
-	# 				current_card_values_list.append(1)
-	# 			elif (sum(current_card_values_list) < 21) and (sum(current_card_values_list) - i) + 11 <= 21:
-	# 				current_card_values_list.remove(i)
-	# 				current_card_values_list.append(11)
-	# 	current_sum = sum(current_card_values_list)
-	# 	player_score.update(current_sum)
-	# 	window["-SCORE-"].update(current_sum)
-	# 	npc_score = "0"
-	# 	window["-NPC-SCORE-"].update(npc_score)
-	# 	for i in range(len(player_cards)):
-	# 		graph.DrawImage(player_cards[i], location=(PLAYER_X, PLAYER_Y))
-	# 		PLAYER_X += 20
+	elif event == '-PLAY-AGAIN-BUTTON-':
+		window["-PLAY-"].update(visible=True)
+		window["-PLAY-AGAIN-BUTTON-"].update(visible=False)
+		graph.erase()
+		DECK = extract_dest_and_values(DECK_LIST)
+		player_cards = []
+		player_score = 0
+		npc_cards = []
+		npc_score = 0
+		PLAYER_X = -40
+		PLAYER_Y = -60
+		NPC_X = -50
+		NPC_Y = 150
+
+		window["-PLAYER-SCORE-"].update(str(f"Your Score:\n\n        {player_score}"))
+		window["-NPC-SCORE-"].update(str(f"Opponent Score:\n\n           {npc_score}"))
 
 window.close()
--- functions.py
+++ functions.py
@@ -5,6 +5,25 @@ def extract_dest_and_values(deck):
 	deck_dict = [deck[i][0] for i in range(52)]
 	return deck_dict
 
+def get_card(deck):
+	selected_card = random.choice(deck)
+	deck.remove(selected_card)
+	return selected_card
+
+def get_score(cards):
+	list_of_card_values = get_card_value(cards)
+	score = sum(check_ace_list_card_values(list_of_card_values))
+	return score
+
+def get_card_value(cards):
+	card_value_list = [cards[val]['value'] for val in range(len(cards))]
+	print(f"Cards Values: {card_value_list}")
+	return card_value_list
+
+def get_card_dest(cards):
+	dest = [cards[des]['dest'] for des in range(len(cards))]
+	return dest
+
 def check_ace(c_value):
 	if c_value == 1 or c_value == 11:
 		return True
@@ -17,7 +36,11 @@ def check_ace_list_card_values(list_of_card_values):
 			list_of_card_values.remove(card_val_local)
 			score_without_ace = sum(list_of_card_values)
 
-			if score_without_ace + 11 <= 21:
+			if score_without_ace + 11 <= 21 and (score_without_ace + 11) > (score_without_ace + 1):
+				list_of_card_values.insert(card_index_local, ace_local[1])
+				print(f"Ace = 11: {list_of_card_values}")
+				return list_of_card_values
+			elif score_without_ace + 11 <= 21:
 				list_of_card_values.insert(card_index_local, ace_local[1])
 				print(f"Ace = 11: {list_of_card_values}")
 				return list_of_card_values
@@ -28,40 +51,6 @@ def check_ace_list_card_values(list_of_card_values):
 		else:
 			print(f"No Ace: {list_of_card_values}")
 			return list_of_card_values
-
-
-
-def get_card(deck):
-	selected_card = random.choice(deck)
-	deck.remove(selected_card)
-	return selected_card
-
-def get_card_value(cards):
-	card_value_list = [cards[val]['value'] for val in range(len(cards))]
-	print(f"Cards Values: {card_value_list}")
-	return card_value_list
-
-def get_card_dest(cards):
-	dest = [cards[des]['dest'] for des in range(len(cards))]
-	return dest
-
-def get_score(cards):
-	list_of_card_values = get_card_value(cards)
-	score = sum(check_ace_list_card_values(list_of_card_values))
-	return score
-
-
-
-
-
-def check_if_blackjack(score):
-	if score == 21:
-		return True
-
-def check_if_bust(player_score, npc_score):
-	if player_score > 21 or npc_score > 21:
-		return True
-	return False
 
 def decide_result(player_score, computer_score):
 	result = ["win", "lose", "draw", "no winner", "blackjack"]
@@ -142,31 +131,4 @@ def declare_result(player_score, computer_score):
 				modal=True
 			)
 
-def npc_hit(card_value_list, deck_dest_list):
-	npc_cards = []
-	npc_index = []
-	npc_card_values_list = []
-	npc_score_number = 0
-	for second_count in range(4):
-		if npc_score_number < 21 and 21 - npc_score_number > 4:
-			npc_cards.append(select_cards(deck_dest_list)[0])
-			npc_index = store_index(npc_cards, deck_dest_list)
-			npc_card_values_list = [card_value_list[i] for i in npc_index]
-			for i in npc_card_values_list:
-				if check_ace(i):
-					if (sum(npc_card_values_list) - i) + 11 > 21:
-						npc_card_values_list.remove(i)
-						npc_card_values_list.append(1)
-					elif (sum(npc_card_values_list) < 21) and (sum(npc_card_values_list) - i) + 11 <= 21:
-						npc_card_values_list.remove(i)
-						npc_card_values_list.append(11)
-			npc_score_number = sum(npc_card_values_list)
-	return npc_cards, npc_score_number, npc_index, npc_card_values_list
 
-def remove_cards(card_list, deck):
-	for card in card_list:
-		deck.remove(card)
-
-def remove_next_card(card_list, deck):
-	for i in range(1):
-		deck.remove(card_list[-1])
